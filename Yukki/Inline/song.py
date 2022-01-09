0728883738b1a24e02211fb3bfa19af9f4645daf
Kyura <96605016+Kyuraxp@users.noreply.github.com>
2022-01-09 22:49:45 +0700

    Update song.py

--- Yukki/Inline/song.py
+++ Yukki/Inline/song.py
@@ -20,7 +20,7 @@ def song_markup(videoid, duration, user_id, query, query_type):
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Search",
+                text="🗑 Tutup Pencarian",
                 callback_data=f"forceclose {query}|{user_id}",
             )
         ],
@@ -32,17 +32,17 @@ def song_download_markup(videoid, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⬇️ Get Audio",
+                text="⬇️ Dapatkan Audio",
                 callback_data=f"gets audio|{videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⬇️ Get Video",
+                text="⬇️ Dapatkan Video",
                 callback_data=f"gets video|{videoid}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Menu",
+                text="🗑 Tutup Menu",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
