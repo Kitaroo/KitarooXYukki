10581262f15019877500b96580e68eeef05f591a
Kitaroo <95475183+Kitaroo@users.noreply.github.com>
2022-02-12 00:23:57 +0700

    Update videocalls.py

--- Yukki/Inline/videocalls.py
+++ Yukki/Inline/videocalls.py
@@ -58,6 +58,12 @@ def stream_quality_markup(videoid, duration, user_id):
         ],
         [
             InlineKeyboardButton(
+                text="⬅️ Kembali",
+                callback_data=f"gback_list_chose_stream {videoid}|{duration}|{user_id}",
+            ),
+        ]
+        [
+            InlineKeyboardButton(
                 text="🗑 Tutup Pencarian",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
